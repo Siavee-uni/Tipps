a114f5bb8c2f26ebe10aed4f5e5e6c3d79259599
Siavee-uni <59541564+Siavee-uni@users.noreply.github.com>
2020-03-11 20:29:33 +0100

    Add files via upload

---
+++ Tipps.php
@@ -0,0 +1,10 @@
+<?php
+
+//Replace simplexml_load_string
+
+$replace = array("  "," ","_",".");
+$vCardUrl = $_SERVER['HTTP_ORIGIN'] . '/signatur/v-cards/' . str_replace($replace,"-",strtolower($pFirstName)) . '-' . strtolower($pName) .'.vcf';
+
+//Echo in php ohne Laravel:
+
+echo '<pre>' . print_r(str_replace($replace,"-",strtolower($pFirstName)), true) . '</pre>';
